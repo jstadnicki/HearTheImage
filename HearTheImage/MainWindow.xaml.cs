960f3630b2f6d58ed3a99d34cadc81e4fdecb457
Marceli Grabowski <marcel.grabowski@outlook.com>
2015-06-13 13:59:14 +0200

    azureblobstorage (get url from local file)

--- HearTheImage/MainWindow.xaml.cs
+++ HearTheImage/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace HearTheImage
 {
@@ -26,7 +16,7 @@ namespace HearTheImage
         }
     }
 
-
+    
     public interface IImageAnalyzer
     {
         Dictionary<StoryImage, List<string>> GetWords(IEnumerable<StoryImage> images);
