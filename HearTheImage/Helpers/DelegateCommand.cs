cc4b256c6046b2c2527e06de362ac6485e94e456
Łukasz Łysyganicz <lysy01@gmail.com>
2015-06-13 13:39:47 +0200

    Helpers for MVVM

---
+++ HearTheImage/Helpers/DelegateCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HearTheImage.Helpers
+{
+    public class DelegateCommand : ICommand
+    {
+        private Func<object, bool> _canExecute = (x) => { return true; } ;
+        private Action<object> _execute = (x) => { };
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            if (execute != null) _execute = execute;
+            if (canExecute != null) _canExecute = canExecute;
+        }
+    }
+}
---
+++ HearTheImage/Helpers/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace HearTheImage.Helpers
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Notify(string propertyName)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
