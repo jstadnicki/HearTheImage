23ac71bd8e4ed3318ab0a6592d81f5c65e639dec
jstadnicki <jaroslaw.stadnicki@gmail.com>
2015-06-13 13:16:43 +0200

    refactored

--- HearTheImage/MainWindow.xaml.cs
+++ HearTheImage/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,29 +32,13 @@ namespace HearTheImage
         Dictionary<StoryImage, List<string>> GetWords(IEnumerable<StoryImage> images);
     }
 
-    public class StoryImage
-    {
-        public FileInfo ImageFile { get; set; }
-    }
-
     public interface ISynonymGenerator
     {
         StoryWord GetSynonyms(string word);
     }
 
-    public class StoryWord
-    {
-        public string Word { get; set; }
-        public List<string> Synonyms { get; set; }
-    }
-
     public interface ISoundsProvider
     {
         StorySound GetSoundForWord(string word);
-    }
-
-    public class StorySound
-    {
-        public FileInfo SoundFile { get; set; } 
     }
 }
---
+++ HearTheImage/StoryWord.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace HearTheImage
+{
+    public class StoryWord
+    {
+        public string Word { get; set; }
+        public List<string> Synonyms { get; set; }
+    }
+}
